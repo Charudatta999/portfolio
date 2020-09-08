ebcf54f4d00ef1ae3ea044cfbb6a4e9ad1faae87
Charudatta <cjawsome999@gmail.com>
2020-09-08 19:36:53 +0530

    final build

---
+++ precache-manifest.40410cceceb94e4fecf5610e9ad3d2c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "490042fe710c9d506e4f44cdf94ac31b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86dfafc3c9016276b8d6",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86dfafc3c9016276b8d6",
+    "url": "/static/js/main.b30b1f18.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
